82f58032ac297cae4d03cfcf4c9e16c2b14ebc75
Mahesh Joshi <maheshjoshi199@gmail.com>
2024-04-16 15:08:07 +0530

    updated readme file

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 14:46:32 2024
+
+@author: mahesh.joshi
+"""
+
+from cnnClassifier import logger
+
+print("Hey this is my first print and logging")
